2ffe6036c5858232240069f1ac609981a40c8cc1
محمدحسین فخرآوری <fakhravary@gmail.com>
2024-11-26 08:13:57 +0330

    them

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fakhravari/Config/LightTheme.dart';
 import 'package:fakhravari/Pages/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: lightTheme,
       locale: Locale('fa', 'IR'),
       builder: (context, widget) {
         return Directionality(
@@ -22,10 +24,6 @@ class MyApp extends StatelessWidget {
       },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: SplashScreen(),
     );
   }
